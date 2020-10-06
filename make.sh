d451e1990497b14cafdd5e1ddcd08e29270a4fb0
ferhatgec <ferhatgectao@gmail.com>
2020-10-06 23:37:55 +0300

    Change make.sh

--- make.sh
+++ make.sh
@@ -3,4 +3,11 @@
 # Required Vala to C compiler, GTK3 library, WebKit2GTK(devel) and GCC (or Clang).
 valac --pkg gtk+-3.0 --pkg webkit2gtk-4.0 Prism.vala -o prism
 
-./prism
+mkdir /usr/share/pixmaps/prism/
+mkdir /usr/share/pixmaps/prism/homepage/
+
+cp resource/*.png /usr/share/pixmaps/prism/
+
+cp resource/homepage/* /usr/share/pixmaps/prism/homepage/
+
+cp prism.desktop /usr/share/applications/
