c95f109e00b2f48729b85adf4d0e390da7acb0ac
ferhatgec <ferhatgectao@gmail.com>
2020-10-21 14:23:39 +0300

    Fix: install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
+
+mkdir $HOME/.config/prism/
+
 # Required Vala to C compiler, GTK3 library, WebKit2GTK(devel) and GCC (or Clang).
-valac --pkg gtk+-3.0 --pkg webkit2gtk-4.0 src/Definitions.vala src/Log.vala src/FileOperations.vala src/EntryCompletion.vala src/Prism.vala -o /bin/prism
+sudo valac --pkg gtk+-3.0 --pkg webkit2gtk-4.0 src/Definitions.vala src/Log.vala src/FileOperations.vala src/EntryCompletion.vala src/Prism.vala -o /bin/prism
 
-mkdir /usr/share/pixmaps/prism/
-mkdir /usr/share/pixmaps/prism/homepage/
+sudo mkdir /usr/share/pixmaps/prism/
+sudo mkdir /usr/share/pixmaps/prism/homepage/
 
-mkdir /home/$USERNAME/.config/prism/
+sudo cp resource/*.png /usr/share/pixmaps/prism/
 
-cp resource/*.png /usr/share/pixmaps/prism/
+sudo cp resource/homepage/* /usr/share/pixmaps/prism/homepage/
 
-cp resource/homepage/* /usr/share/pixmaps/prism/homepage/
-
-cp prism.desktop /usr/share/applications/
+sudo cp prism.desktop /usr/share/applications/
